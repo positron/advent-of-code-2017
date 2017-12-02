c1d4690c240157469c37239649ac7ec23be6f631
Philip Jagielski <philip.jagielski@climate.com>
2017-12-01 22:24:51 -0600

    Day 1 Puzzle 2: halfway through the list

--- src/advent/day1_captcha.clj
+++ src/advent/day1_captcha.clj
@@ -10,14 +10,33 @@
        char-array
        (map #(Character/getNumericValue %))))
 
-(defn solve-captcha
-  [captcha]
-  (->> (conj captcha (first captcha))
-       (partition 2 1)
+(defn- add-matching-numbers
+  [number-pairs]
+  (->> number-pairs
        (filter #(apply = %))
        (map first)
        (apply +)))
 
+(defn solve-captcha-1
+  [captcha]
+  (->> (conj captcha (first captcha))
+       (partition 2 1)
+       (add-matching-numbers)))
+
+(defn solve-captcha-2
+  [captcha]
+  (let [captcha (vec captcha)
+        half (/ (count captcha) 2)]
+    (->> captcha
+         (partition half)
+         (apply map vector) ; zip up the two lists
+         add-matching-numbers
+         (* 2)))) ; going through the second half of the list will have the same result as the first half
+
 ; Puzzle 1
-; (-> (read-captcha) solve-captcha)
+; (-> (read-captcha) solve-captcha-1)
 ; => 1182
+
+; Puzzle 2
+; (-> (read-captcha) solve-captcha-2)
+; => 1152
