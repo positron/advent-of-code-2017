c3b39d9b04a7a2316d56676ae69fc6e15f4037f7
Philip Jagielski <philip.jagielski@climate.com>
2017-12-01 22:07:11 -0600

    Add project boilerplate

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2017 Advent of Code in Clojure
+http://adventofcode.com/2017
+
+I probably won't do it all. Fun though!
+
+Each file is self contained and only uses the standard library.
+
+No formal tests since each day is simple enough to build up the given test cases in the repl as I go.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject advent "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]]
+  :target-path "target/%s")
