94eccd161a84169e04d3aab8be64b3302da56f78
Philip Jagielski <philip.jagielski@climate.com>
2017-12-02 22:51:25 -0600

    Day 2 Puzzle 2: division

--- src/advent/day2_corruption.clj
+++ src/advent/day2_corruption.clj
@@ -14,17 +14,34 @@
         (map (fn [row]
                (map #(Integer/parseInt %) row))))))
 
-(defn- calculate-diff
+(defn calculate-diff
   "Returns the difference between the highest and lowest values in a list"
-  [l]
-  (- (apply max l) (apply min l)))
+  [nums]
+  (- (apply max nums) (apply min nums)))
 
-(defn calculate-checksum
-  [spreadsheet]
+(defn calculate-division-result
+  "Given a list of numbers, find the two numbers that evenly divide and return the result"
+  [nums]
+  (letfn [(divisible? [a b]
+            (let [res (/ a b)]
+              (when (and (not= 1 res)
+                         (int? res))
+                    res)))
+          (find-divisible [num]
+            (some (partial divisible? num) nums))]
+    ; Too lazy to not make this O(n^2) brute force since n is so small
+    (some find-divisible nums)))
+
+(defn sum-results
+  [spreadsheet row-fn]
   (->> spreadsheet
-       (map calculate-diff)
+       (map row-fn)
        (apply +)))
 
 ; Puzzle 1
-; (-> (read-spreadsheet) calculate-checksum)
+; (-> (read-spreadsheet) (sum-results calculate-diff))
 ; => 46402
+
+; Puzzle 2
+; (-> (read-spreadsheet) (sum-results calculate-division-result))
+; => 265
